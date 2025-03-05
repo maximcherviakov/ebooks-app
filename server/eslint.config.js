e8b1db752eff77ac713c76e8ca1170f9071a0916
maximcherviakov <cherviakovmaxim@gmail.com>
2025-03-05 23:25:48 +0200

    Refactor ESLint configuration to use languageOptions for environment settings

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+  languageOptions: {
+    globals: {
+      browser: true,
+      node: true,
+      es2021: true,
+    },
   },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
