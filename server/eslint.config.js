6266f220b18ac0bb1e0ee4adc4bd5423d2d9cff6
maximcherviakov <cherviakovmaxim@gmail.com>
2025-03-05 23:30:57 +0200

    Refactor ESLint configuration to use ES module syntax and improve TypeScript support

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,28 +1,23 @@
-module.exports = [
-  {
-    languageOptions: {
-      globals: {
-        browser: true,
-        node: true,
-        es2021: true,
-      },
-      ecmaVersion: 12,
-      sourceType: "module",
-    },
-    plugins: {
-      "@typescript-eslint": {
-        rules: {
-          "@typescript-eslint/indent": ["error", 2],
-          "@typescript-eslint/linebreak-style": ["error", "unix"],
-          "@typescript-eslint/quotes": ["error", "single"],
-          "@typescript-eslint/semi": ["error", "always"],
-        },
-      },
-    },
-  },
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  ts.configs.recommended,
   {
     files: ["**/*.ts", "**/*.tsx"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-    parser: "@typescript-eslint/parser",
+    languageOptions: {
+      parser: tsParser,
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+    plugins: { "@typescript-eslint": ts },
+    rules: {
+      "@typescript-eslint/indent": ["error", 2],
+      "@typescript-eslint/linebreak-style": ["error", "unix"],
+      "@typescript-eslint/quotes": ["error", "single"],
+      "@typescript-eslint/semi": ["error", "always"],
+    },
   },
 ];
