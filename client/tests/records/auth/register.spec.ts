6044711c07cf5dd34b72ed16f92759953941d2e6
maximcherviakov <cherviakovmaxim@gmail.com>
2025-03-11 21:04:40 +0200

    Changed directory

--- client/tests/records/auth/register.spec.ts
+++ client/tests/records/auth/register.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker";
-import { config } from "../config/config";
+import { config } from "../../config/config";
 
 test.describe("Registration", () => {
   test("Register user with valid data", async ({ page }) => {
